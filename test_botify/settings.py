dbf2ecf5e58d5731b10787fea8f25a864a7e91eb
zelazna <constantin.guidon@gmail.com>
2019-04-18 10:36:16 +0200

    [feature] add filtering and ordering capabilities

--- test_botify/settings.py
+++ test_botify/settings.py
@@ -29,6 +29,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter'
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 40
 }
@@ -41,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'towns',
-    'rest_framework'
+    'rest_framework',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
--- towns/views/town_list.py
+++ towns/views/town_list.py
@@ -7,3 +7,6 @@ from towns.serializers.town_serializer import TownSerializer
 class TownList(generics.ListAPIView):
     queryset = Town.objects.all()
     serializer_class = TownSerializer
+    search_fields = ('name',)
+    ordering_fields = ('population', 'name')
+    filterset_fields = {'population': ['gte', 'lte']}
