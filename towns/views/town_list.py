2c9f78d6a4eccdc6378adfa0e865c74207d5974c
zelazna <constantin.guidon@gmail.com>
2019-04-18 10:03:57 +0200

    [feature] global pagination

--- towns/views/town_list.py
+++ towns/views/town_list.py
@@ -1,13 +1,9 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from towns.models import Town
 from towns.serializers.town_serializer import TownSerializer
 
 
-class TownList(APIView):
-    @staticmethod
-    def get(request):
-        snippets = Town.objects.all()
-        serializer = TownSerializer(snippets, many=True)
-        return Response(serializer.data)
+class TownList(generics.ListAPIView):
+    queryset = Town.objects.all()
+    serializer_class = TownSerializer
